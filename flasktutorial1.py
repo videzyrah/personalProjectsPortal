4b7b3b594c866244be242de96f51302e2e13c686
Greg Stull <greg.t.stull@my.sfcollege.edu>
2016-12-29 10:35:48 -0500

    english

--- flasktutorial1.py
+++ flasktutorial1.py
@@ -6,9 +6,9 @@ app=Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/about/')
-def about():
-    return render_template("about.html")
+@app.route('/diy/')
+def diy():
+    return render_template("diy.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
