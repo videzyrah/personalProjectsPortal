102cb4f5a1d3d21f468aa0e4bba033689db47503
videzyrah <videzyrah@gmail.com>
2020-04-16 19:50:42 -0400

    flask fuckery

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/diy/')
+def diy():
+    return render_template("diy.html")
+
+@app.route('/basslines/')
+def basslines():
+    return render_template("basslines.html")
+
+@app.route('/ppp/')
+def ppp():
+    return render_template("ppp.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
