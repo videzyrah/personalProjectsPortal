ee9599968b9605a7cd1a4b941193fd3284298daf
videzyrah <videzyrah@gmail.com>
2020-04-18 02:32:41 -0400

    anarchademics routed

--- flasktutorial1.py
+++ flasktutorial1.py
@@ -18,5 +18,9 @@ def basslines():
 def ppp():
     return render_template("ppp.html")
 
+@app.route('/anarchademics')
+def ppp():
+    return render_template("anarchademics.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
